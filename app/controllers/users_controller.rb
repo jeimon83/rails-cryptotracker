349b09eda28d390ea46a94dee10884384c3e2e97
jaime-loopstudio <jgarcia@loopstudio.dev>
2021-11-20 19:45:41 -0300

    clean user edit action controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,7 @@ class UsersController < ApplicationController
 
   def edit
     if params.present? && params[:commit] == 'Save'
-      user = params[:user]
-      current_user.update(firstname: user[:firstname], lastname: user[:lastname])
-      Wallet.find_or_create_by(name: user[:wallet_name], address: user[:wallet_address], user: current_user)
-      Exchange.find_or_create_by(name: user[:exchange_name], api_key: user[:exchange_api_key], secret_key: user[:exchange_secret_key], user: current_user)
+      current_user.update_info(params[:user]) 
       flash[:notice] = 'User updated'
       redirect_to edit_user_path(current_user)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,22 @@ class User < ApplicationRecord
   has_one :wallet
   has_one :exchange
   has_many :balances
+
+  def create_or_update_wallet(wallet_attr)
+    return wallet.update(wallet_attr) if wallet.present?
+    
+    wallet.create(wallet_attr)
+  end
+
+  def create_or_update_exchange(exchange_attr)
+    return exchange.update(exchange_attr) if exchange.present?
+    
+    exchange.create(exchange_attr)
+  end
+
+  def update_info(data)
+    update(firstname: data[:firstname], lastname: data[:lastname])
+    create_or_update_wallet({ name: data[:wallet_name], address: data[:wallet_address] })
+    create_or_update_exchange({ name: data[:exchange_name], api_key: data[:exchange_api_key], secret_key: data[:exchange_secret_key] })
+  end
 end
